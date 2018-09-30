fa301bbba7fe2c0872347efa692fc67f3a8d6098
prototypez <prototypez@126.com>
2018-09-30 11:26:49 +0800

    feat(plugin): asm get all module information now

--- plugin/src/main/groovy/io/github/prototypez/appjoint/plugin/AppJointTransform.groovy
+++ plugin/src/main/groovy/io/github/prototypez/appjoint/plugin/AppJointTransform.groovy
@@ -14,12 +14,29 @@ import org.gradle.api.Project
 import org.objectweb.asm.AnnotationVisitor
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassVisitor
-import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes
 
 class AppJointTransform extends Transform {
 
     Project mProject
+
+    /**
+     * Classes annotated with @ModuleSpec
+     */
+    def moduleApplications = []
+    /**
+     * Classes annotated with @ModulesSpec
+     */
+    def appApplications = [:]
+    /**
+     * Classes annotated with @RouterProvider
+     */
+    def routerAndImpl = [:]
+
+    /**
+     * The AppJoint class File
+     */
+    def appJointClassFile
 
     AppJointTransform(Project project) {
         mProject = project
@@ -70,7 +87,7 @@ class AppJointTransform extends Transform {
                 if (jarName == ":core") {
                     // maybe stub in dev and handle them later
                     possibleStubs.add(jarInput)
-                    // maybe submodule
+                    // maybe submodule, ':core' could be user's business module
                     possibleModules.add(jarInput)
                 } else if (jarName.startsWith(":")) {
                     // maybe submodule
@@ -85,6 +102,8 @@ class AppJointTransform extends Transform {
                 }
             }
 
+            // Inside submodules, find class annotated with
+            // @ModuleSpec, @ModulesSpec or @RouterProvider
             possibleModules.each {jarInput ->
                 def jarName = jarInput.name
 
@@ -105,7 +124,7 @@ class AppJointTransform extends Transform {
                 FileUtils.copyDirectory(unzipDir, repackageFolder)
 
                 unzipDir.eachFileRecurse(FileType.FILES) { File it ->
-                    checkAndTransformClass(it)
+                    findAnnotatedClasses(it)
                 }
 
                 // re-package the folder to jar
@@ -116,6 +135,40 @@ class AppJointTransform extends Transform {
                 zc.compress(repackageFolder.getAbsolutePath())
             }
 
+            // Inside local ':core' Module or remote app-joint dependency,
+            // find the AppJoint class in jars
+            possibleStubs.each {jarInput ->
+                def jarName = jarInput.name
+
+                File unzipDir = new File(
+                        jarInput.file.getParent(),
+                        jarName.replace(":", "") + "_unzip")
+                if (unzipDir.exists()) {
+                    unzipDir.delete()
+                }
+                unzipDir.mkdirs()
+                Decompression.uncompress(jarInput.file, unzipDir)
+
+                File repackageFolder = new File(
+                        jarInput.file.getParent(),
+                        jarName.replace(":", "") + "_repackage"
+                )
+
+                FileUtils.copyDirectory(unzipDir, repackageFolder)
+
+                unzipDir.eachFileRecurse(FileType.FILES) { File it ->
+                    findAppJointClass(it)
+                }
+
+                // re-package the folder to jar
+                def dest = transformInvocation.outputProvider.getContentLocation(
+                        jarName, jarInput.contentTypes, jarInput.scopes, Format.JAR)
+
+                Compressor zc = new Compressor(dest.getAbsolutePath())
+                zc.compress(repackageFolder.getAbsolutePath())
+            }
+
+            // Find annotated classes and AppJoint class in dir
             input.directoryInputs.each { dirInput ->
                 def outDir = transformInvocation.outputProvider.getContentLocation(dirInput.name, dirInput.contentTypes, dirInput.scopes, Format.DIRECTORY)
                 // dirInput.file is like "build/intermediates/classes/debug"
@@ -127,6 +180,8 @@ class AppJointTransform extends Transform {
                         if (it.isDirectory()) {
                             new File(outDir, path).mkdirs()
                         } else {
+                            findAnnotatedClasses(it)
+                            findAppJointClass(it)
                             new File(outDir, path).bytes = it.bytes
                         }
                     }
@@ -138,11 +193,33 @@ class AppJointTransform extends Transform {
                     dirInput.changedFiles.keySet().each(callback)
                 }
             }
+
+            mProject.logger.info("moduleApplications: $moduleApplications")
+            mProject.logger.info("appApplications: $appApplications")
+            mProject.logger.info("routerAndImpl: $routerAndImpl")
+            mProject.logger.info("appJointClassFile: $appJointClassFile")
         }
     }
 
+    // Find the AppJoint class
+    void findAppJointClass(File file) {
+        if (!file.exists() || !file.name.endsWith(".class")) {
+            return
+        }
+        ClassReader cr = new ClassReader(new FileInputStream(file))
+        cr.accept(new ClassVisitor(Opcodes.ASM5) {
+            @Override
+            void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+                super.visit(version, access, name, signature, superName, interfaces)
+                if (name == "io/github/prototypez/appjoint/AppJoint") {
+                    appJointClassFile = file
+                }
+            }
+        }, 0)
+    }
+
     // Check @ModuleSpec, @ModulesSpec, @RouterProvider existence
-    void checkAndTransformClass (File file) {
+    void findAnnotatedClasses(File file) {
         if (!file.exists() || !file.name.endsWith(".class")) {
             return
         }
@@ -150,9 +227,20 @@ class AppJointTransform extends Transform {
         cr.accept(new ClassVisitor(Opcodes.ASM5) {
             @Override
             AnnotationVisitor visitAnnotation(String desc, boolean visible) {
-                mProject.logger.info("visit annotation:" + desc)
+                mProject.logger.info("visiting $desc")
+                switch (desc) {
+                    case "Lio/github/prototypez/appjoint/core/ModuleSpec;":
+                        moduleApplications.add(cr.className)
+                        break
+                    case "Lio/github/prototypez/appjoint/core/ModulesSpec;":
+                        appApplications[cr.className] = file
+                        break
+                    case "Lio/github/prototypez/appjoint/core/RouterProvider;":
+                        cr.interfaces.each { routerAndImpl[it] = cr.className }
+                        break
+                }
+                return super.visitAnnotation(desc, visible)
             }
         }, 0)
     }
-
 }
